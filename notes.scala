9eca173e6c6f7100614a420fb56b0d1957fb51a8
recotra <rrrrrr8@users.noreply.github.com>
2019-05-10 09:55:26 +0800

    Create notes.scala

---
+++ notes.scala
@@ -0,0 +1,7 @@
+1. call-by-name, lazy evaluation
+   def log(m: String) = if (logEnabled) println(m)
+   
+   def log(m: => String) = if (logEnabled) println(m) // call-by-name, m is not evaluated if logEnabled is false
+   
+   
+   
